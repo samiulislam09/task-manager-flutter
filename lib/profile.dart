f7f4599fb94a3e33b35d66514f3b922ac60c6f36
samiulislam09 <samiulislamsawon09@gmail.com>
2025-01-05 21:52:25 +0600

    added tasks section

---
+++ lib/profile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Row(
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/profile.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            Text(
+              'Hello, John!',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 113, 113, 113),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("Profile"),
+      ),
+    );
+  }
+}
